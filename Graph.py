661ef163661e5f2e6abf8fa5eec82a8f5fb72cbd
Danibol <danail123451989@gmail.com>
2026-03-12 10:47:34 +0200

    - Implement functions for building "Small World" and "Scale-Free" topologies

--- Graph.py
+++ Graph.py
@@ -1,14 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+
+# TO-DO check add/remove edge functions. DO they keep the grpah consistent. Storing (i,j)/(j,i) as edges. IMORTANT!
 class Graph:
     
     def __init__(self, n_nodes):
         self.n_nodes = n_nodes
         self.A = np.zeros((n_nodes, n_nodes), dtype=int)  # adjacency matrix
+        # Keep edges list also. We need to be able to quckly pick random edge. Only adj matrix  makes it very slow. And multiple runs take too much time
+        self.edges = []  
     
     def add_edge(self, i, j):
-        self.A[i][j] = 1
-        self.A[j][i] = 1
+        if self.A[i, j] == 1:
+            return
+
+        self.A[i, j] = 1
+        self.A[j, i] = 1
+
+        self.edges.append((i, j))
     
     def neighbors(self, i):
         connected = self.A[i] == 1
@@ -33,7 +43,13 @@ class Graph:
                 queue.extend(self.neighbors(node))
         return len(visited) == self.n_nodes
 
+    def remove_edge(self, i, j):
+        if self.A[i,j] == 0:
+            return
 
+        self.A[i,j] = 0
+        self.A[j,i] = 0
+        self.edges.remove((i,j)) if (i,j) in self.edges else self.edges.remove((j,i))
 
 def get_fully_connected(n):
     #Every agent connected to every other agent
@@ -41,4 +57,66 @@ def get_fully_connected(n):
     for i in range(n):
         for j in range(i+1, n):
             G.add_edge(i, j)
+    return G
+
+# Watts-Strogatz network
+def get_small_world(n, k=4, p=0.1, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    G = Graph(n)
+
+    # Create a ring
+    # For each node, connect it to k/2 neighbours on left and on right
+    for i in range(n):
+        for j in range(1, k // 2 + 1):
+            G.add_edge(i, (i + j) % n)
+            G.add_edge(i, (i - j) % n)
+
+    # Rewire edges
+    for i in range(n):
+        for j in range(1, k // 2 + 1):
+            
+            # rewire with probability = p
+            if np.random.random() < p:
+
+                # original neighbor in the ring
+                neighbour = (i + j) % n
+
+                # Remove the original local edge
+                # We have to do it to so 
+                G.remove_edge(i, neighbour)
+
+                # Node thats not already connected to i
+                candidates = [x for x in range(n) if x != i and G.A[i][x] == 0]
+
+                # Add random candidate
+                if candidates:
+                    new_neighbour = np.random.choice(candidates)
+                    G.add_edge(i, new_neighbour)
+    return G
+
+
+#Barabasi-Albert
+def get_scale_free(n, m=2, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    G = Graph(n)
+
+    # Create intital fully connected subgraph
+    for i in range(m):
+        for j in range(i + 1, m + 1):
+            G.add_edge(i, j)
+
+    # adding new notes
+    for new_node in range(m + 1, n):
+        # Compute degrees of all existing nodes
+        degrees = np.array([G.degree(i) for i in range(new_node)])
+        # Convert degrees into probabilities
+        probs = degrees / degrees.sum()
+
+        # Select m target nodes with preference to the more "popular"
+        targets = np.random.choice(new_node,size=m, replace=False, p=probs)
+        for target in targets:
+            G.add_edge(new_node, target)
     return G
