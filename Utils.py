8e58b4ed9213af3ae27ee466a24415a23b40acea
Danibol <danail123451989@gmail.com>
2026-03-15 14:51:06 +0200

    - Merge util files, document code, change the structure in the main notebook a bit

--- Utils.py
+++ Utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+This module contains utility functions that does not necesseruly has much in common, but would be overcomplication to separate them into files.
+"""
+
 def get_clusters(opinions, epsilon=0.5):
     """
     Identify opinion clusters based on a confidence bound.
@@ -106,5 +110,39 @@ def plot_epsilon_sweep(results, title=""):
 
     if title:
         plt.suptitle(title)
+    plt.tight_layout()
+    plt.show()
+
+
+#Visualizes how different evolutions compare. Useful for comparing the results of a given experiment
+def plot_evolution_comparison(histories, titles, suptitle=""):
+    """
+    Parameters:
+    -----------
+    histories        - list of hitories of the simulations
+    titles           - list of titles for each plot
+    """
+    alpha=0.4
+    line_width=0.8
+    font_size=13
+    n = len(histories)
+    fig, axes = plt.subplots(1, n, figsize=(6*n, 5), sharey=True)
+    
+    if n == 1:
+        axes = [axes]
+    
+    for ax, history, title in zip(axes, histories, titles):
+        n_agents = history.shape[1]
+        for i in range(n_agents):
+            ax.plot(history[:, i], alpha=alpha, linewidth=line_width)
+        ax.set_title(title)
+        ax.set_xlabel('Time Step')
+        ax.set_ylim(-0.05, 1.05)
+    
+    axes[0].set_ylabel('Opinion')
+    
+    if suptitle:
+        plt.suptitle(suptitle, fontsize=font_size)
+    
     plt.tight_layout()
     plt.show()
